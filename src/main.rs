ddfe1bec97b37d201061b516b8b86645f955e52a
memoryleak47 <memoryleak47@protonmail.ch>
2020-03-17 02:04:30 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use std::fs::File;
+use std::io::Write;
+use std::process::Command;
+
 mod term;
 mod parse;
 
 use term::Term;
 
 fn main() {
-	dbg!(Term::parse("\\x.x"));
+	let t = Term::parse("\\x.x");
+	let s = t.to_llvm_ir();
+
+	let mut file = File::create("foo.ll").unwrap();
+	file.write_all(s.as_bytes()).unwrap();
+
+	println!("{:?}", Command::new("clang")
+		.arg("foo.ll")
+		.output()
+		.unwrap());
 }
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -17,7 +17,7 @@ fn eat_application(tokens: &[Token]) -> (&[Token], Term) {
 }
 
 fn eat_abstraction(tokens: &[Token]) -> (&[Token], Term) {
-	if let &[Token::Lambda, Token::Var(ref x), Token::Dot, ref rest @ ..] = tokens {
+	if let [Token::Lambda, Token::Var(ref x), Token::Dot, ref rest @ ..] = tokens {
 		let (rest, term) = eat_term(rest);
 		return (rest, Term::Abstraction(x.clone(), Box::new(term)));
 	}
@@ -25,7 +25,7 @@ fn eat_abstraction(tokens: &[Token]) -> (&[Token], Term) {
 }
 
 fn eat_term(tokens: &[Token]) -> (&[Token], Term) {
-	assert!(tokens.len() > 0);
+	assert!(!tokens.is_empty());
 	match tokens[0] {
 		Token::LeftParen => eat_application(tokens),
 		Token::Lambda => eat_abstraction(tokens),
